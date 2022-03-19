e62e9ee7b884b5f491a68c81d52480c829fe0ee4
amalkurup1729 <amal.kurup1@gmail.com>
2022-03-19 22:39:36 +0530

    The Disclosure News App

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem mhome,mscience,mhealth,mtech,mentertainment,msports;
+    PagerAdapter pagerAdapter;
+    Toolbar mtoolbar;
+    String api="0c99f1de120f43b6a26f55ae2d3ad53b";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mtoolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+        mhome=findViewById(R.id.home);
+        mscience=findViewById(R.id.science);
+        msports=findViewById(R.id.sports);
+        mhealth=findViewById(R.id.health);
+        mentertainment=findViewById(R.id.entertainment);
+        mtech=findViewById(R.id.technology);
+
+        ViewPager viewPager=findViewById(R.id.fragmentcontainer);
+        tabLayout=findViewById(R.id.include);
+
+        pagerAdapter=new PagerAdapter(getSupportFragmentManager(),6);
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+             viewPager.setCurrentItem(tab.getPosition());
+             if(tab.getPosition()==0||tab.getPosition()==1||tab.getPosition()==2||tab.getPosition()==3||tab.getPosition()==4||tab.getPosition()==5)
+             {
+             pagerAdapter.notifyDataSetChanged();
+             }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent abc = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(abc);
+                finish();
+            }
+        },5000);
+
+    }
+}
